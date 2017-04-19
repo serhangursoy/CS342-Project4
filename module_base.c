d6cb4245ffd2601f96f07d5e9d3cb561532d67cc
Dell <gursoyserhan@yandex.com>
2017-04-19 05:40:52 +0300

    Hotfix

--- module_base.c
+++ module_base.c
@@ -5,6 +5,7 @@
 #include <linux/kernel.h>
 #include <asm/uaccess.h>
 #include <linux/fs.h> 
+#include <linux/file.h>
 #include <linux/cdev.h>
 #include <linux/proc_fs.h>
 #include <linux/pid.h>
@@ -41,46 +42,47 @@ static int hello_init(void)
 	short checkerByte = 0;
 	for_each_process(temp) {
 		if (temp->pid == input_pid) {
+
 			printk(KERN_WARNING "This process's name is %s\nParent process's name is %s  with pid %i\n", temp->comm, temp->parent->comm, temp->parent->pid);
 
 			printk(KERN_WARNING "|>%s pid:%i\n", temp->parent->comm, temp->parent->pid);
 
-	 	struct list_head *p; //Pointer
-	 	struct task_struct child_task;
-	 	list_for_each(p, &(temp->parent->children)){
-	 		child_task = *list_entry(p, struct task_struct, sibling);
-	 		if (child_task.pid != input_pid)
-	 			printk("      |-->%s pid:%d\n",child_task.comm, child_task.pid);
-	 		else{
-	 			printk("      |-->%s pid:%d\n",child_task.comm, child_task.pid);
-	 			struct list_head *p2; //Pointer
-	 			struct task_struct child_task2;
-	 			list_for_each(p2, &(temp->children)){
-	 				child_task2 = *list_entry(p2, struct task_struct, sibling);
-	 				if (child_task2.pid != input_pid)
-	 					printk("          |-->%s pid:%d\n",child_task2.comm, child_task2.pid);
+	 		struct list_head *p; //Pointer
+	 		struct task_struct child_task;
+	 		list_for_each(p, &(temp->parent->children)){
+	 			child_task = *list_entry(p, struct task_struct, sibling);
+	 			if (child_task.pid != input_pid)
+	 				printk("      |-->%s pid:%d\n",child_task.comm, child_task.pid);
+	 			else{
+	 				printk("      |-->%s pid:%d\n",child_task.comm, child_task.pid);
+	 				struct list_head *p2; //Pointer
+	 				struct task_struct child_task2;
+	 				list_for_each(p2, &(temp->children)){
+	 					child_task2 = *list_entry(p2, struct task_struct, sibling);
+	 					if (child_task2.pid != input_pid)
+	 						printk("          |-->%s pid:%d\n",child_task2.comm, child_task2.pid);
+	 				}
 	 			}
 	 		}
-	 	}
 
-	 	checkerByte = 1;
+	 		checkerByte = 1;
 
 
-	 	struct mm_struct *m;
-	 	struct vm_area_struct *v;
-	 	m = temp->mm;
-	 	v = m->mmap;
-	 	long totalSize = 0;
+	 		struct mm_struct *m;
+	 		struct vm_area_struct *v;
+	 		m = temp->mm;
+	 		v = m->mmap;
+	 		long totalSize = 0;
 
-	 	while(v != NULL) {
-	 		printk("VM_START: %ld, VM_END: %ld\n", v->vm_start, v->vm_end);
-	 		totalSize += v->vm_end - v->vm_start;
-	 		v = v->vm_next;
-	 	}
+	 		while(v != NULL) {
+	 			printk("VM_START: %ld, VM_END: %ld\n", v->vm_start, v->vm_end);
+	 			totalSize += v->vm_end - v->vm_start;
+	 			v = v->vm_next;
+	 		}
 
-	 	printk("Total VM size, %ld byte\n", totalSize);
+	 		printk("Total VM size, %ld byte\n", totalSize);
 
-	 	
+	 		
 // This part goes into kernel panic. Yeah, you heard it right, I said kernel panic.
 /*
 	 	struct files_struct *files_task;
@@ -96,20 +98,20 @@ static int hello_init(void)
 	 		count++;
 	 	}
 */
+	 	}
 	 }
+
+	 if (checkerByte==0)
+	 	printk(KERN_ERR "Couldn't find process with pid %i, are you sure it's exists?\n", input_pid);
+
+	 return 0;
 	}
 
-	if (checkerByte==0)
-		printk(KERN_ERR "Couldn't find process with pid %i, are you sure it's exists?\n", input_pid);
-
-	return 0;
-}
-
-static void hello_exit(void)
-{
-	printk(KERN_INFO "************ END OF MODULE ************\n");
-}
+	static void hello_exit(void)
+	{
+		printk(KERN_INFO "************ END OF MODULE ************\n");
+	}
 
 
-module_init(hello_init);
-module_exit(hello_exit);
+	module_init(hello_init);
+	module_exit(hello_exit);
